9b5e2ad4436446bc88e039bfdb89f0a9d1115ccc
kisshc <kisshc@kisshc.com>
2014-11-06 21:56:29 +0800

    Change control

--- relay-controller.js
+++ relay-controller.js
@@ -4,17 +4,17 @@ var _path = "/sys/devices/virtual/gpio/",
 	
 var _read = function(gpio,callback){
 	/*
-	fs.readFile(_path + "gpio" + gpio + "/direction",function(err,data){
+	fs.readFile(_path + "gpio" + gpio + "/value",function(err,data){
 		if(err) 
 			callback(null);
 		else if(typeof(callback) == "function")
-			callback(data.toString().replace('\n',""));
+			callback(parseInt(data));
 	})
 	*/
 	try{
-		var data = fs.readFileSync(_path + "gpio" + gpio + "/direction");
+		var data = fs.readFileSync(_path + "gpio" + gpio + "/value");
 		if(typeof(callback) == "function")
-			callback(data.toString().replace('\n',""));
+			callback(parseInt(data));
 	}catch(e){
 		callback(null);
 	}
@@ -22,28 +22,38 @@ var _read = function(gpio,callback){
 
 var _write = function(gpio,status,callback){
 	/*
-	fs.writeFile(_path + "gpio" + gpio + "/direction",status,function(err){
+	fs.writeFile(_path + "gpio" + gpio + "/value",status,function(err){
 		if(err)
 			console.info(err);
 		else if(typeof(callback) == "function")
 			callback(status);
 		
-		if(status == "in")
+		if(status == 1)
 			_unexport(gpio);
 	})
 	*/
 	try{
-		fs.writeFileSync(_path + "gpio" + gpio + "/direction",status);
-		if(status == "in"){
+		
+		if(relay.direction == null || relay.direction == "in"){
+			var direction = fs.readFileSync(_path + "gpio" + gpio + "/direction");
+			if(direction.toString().replace("\n","") == "in"){
+				fs.writeFileSync(_path + "gpio" + gpio + "/direction","out");
+			}
+			relay.direction = "out";
+		}
+		
+		fs.writeFileSync(_path + "gpio" + gpio + "/value",status);
+		if(status == 1){
 			_unexport(gpio,function(){
 				if(typeof(callback) == "function")
 					callback(status);			
 			});	
 		}else{
-			callback(status);
+			if(typeof(callback) == "function")
+				callback(status);
 		}
 	}catch(e){
-		console.error("Write Error " + status);
+		console.error("Write Error ");
 	}
 }
 
@@ -67,43 +77,84 @@ var _unexport = function(gpio,callback){
 }
 
 var relay = {
-
-	//get relay status in or out
+	
+	direction:null,
+	
+	//get relay status 1 or 0
 	getStatus:function(gpio,callback){
 		_read(gpio,callback);
 	},
 	
-	//open out
-	open:function(gpio,callback){
-		this.getStatus(gpio,function(status){
-			if(status == null){
-				_export(gpio,function(){
-					_write(gpio,"out",callback);
-				});
-			}
-			if(status == "in")
-				_write(gpio,"out",callback);
-				
-		})
+	//open 0
+	open:function(gpio,times,callback){
+		if(typeof(times) == "function" || times == undefined){
+			callback = times;
+			relay.getStatus(gpio,function(status){
+				if(status == null){
+					_export(gpio,function(){
+						_write(gpio,0,callback);
+					});
+				}
+				if(status == 1)
+					_write(gpio,0,callback);
+					
+			})			
+		}else{
+			relay.openAt(gpio,times,callback);
+		}
 	},
 	
-	//close in
-	close:function(gpio,callback){
-		this.getStatus(gpio,function(status){
-			if(status == "out")
-				_write(gpio,"in",callback);
-		})
+	//close 1
+	close:function(gpio,times,callback){
+		if(typeof(times) == "function" || times == undefined){
+			callback = times;
+			relay.getStatus(gpio,function(status){
+				if(status == 0)
+					_write(gpio,1,callback);
+			})		
+		}else{
+			relay.closeAt(gpio,times,callback);
+		}
 	},
 
-	//toggle in <=> out
+	//toggle 1 <=> 0
 	toggle:function(gpio,callback){
 		_read(gpio,function(status){
-			if(status == null || status == "in"){
+			if(status == null || status == 1){
 				relay.open(gpio,callback);
 			}else{
 				relay.close(gpio,callback);
 			}
 		})		
+	},
+	
+	//setTime to open
+	openAt:function(gpio,times,callback){
+		setTimeout(function(){
+			relay.open(gpio,callback);
+		},times);
+	},
+	
+	//setTime to close
+	closeAt:function(gpio,times,callback){
+		setTimeout(function(){
+			relay.close(gpio,callback);
+		},times);
+	},
+
+	//toggle Time 
+	toggleAt:function(gpio,obj,callback){
+		if(typeof(obj) == "object"){
+			var i = 0;
+			var times = setInterval(function(){
+				relay.toggle(gpio,callback);
+				i++;
+				if(i == obj.number)
+					clearInterval(times);
+			},obj.time);			
+		}else{
+			relay.toggle(gpio,callback);
+		}		
 	}
 
 }
